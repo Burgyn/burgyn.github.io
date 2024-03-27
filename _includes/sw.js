a71eeecd142e0938e857e002b859ac548fec70b8
Burgyn <mino.martiniak@gmail.com>
2024-03-27 21:38:29 +0100

    offline mode

--- _includes/sw.js
+++ _includes/sw.js
@@ -1,44 +1,57 @@
 const cacheName = 'burgyn-{{ site.cache_version }}';
+const offlineUrl = 'offline.html';
 
-// Install event
 self.addEventListener('install', evt => {
+    self.skipWaiting(); // Prevezme kontrolu ihneď po inštalácii
     evt.waitUntil(
         caches.open(cacheName).then(cache => {
             return cache.addAll([
+                offlineUrl,
                 '/',
                 '/about/',
+                '/assets/css/main.css',
+                '/assets/main.css',
+                '/assets/images/cover.gif',
             ]);
         })
     );
 });
 
 self.addEventListener('activate', evt => {
-    console.log('service worker has been activated');
     evt.waitUntil(
         caches.keys().then(keys => {
             return Promise.all(keys
                 .filter(key => key !== cacheName)
-                .map(key => {
-                    console.log('deleting cache', key);
-                    return caches.delete(key);
-                
-                })
+                .map(key => caches.delete(key))
             );
+        }).then(() => {
+            self.clients.claim();
         })
     );
 });
 
-self.addEventListener('fetch', evt => {
-    if (!evt.request.url.includes('manifest.json') && !evt.request.url.endsWith('sw.js')) {
-        evt.respondWith(
-            caches.match(evt.request).then(cacheRes => {
-                return cacheRes || fetch(evt.request).then(fetchRes => {
-                    return caches.open(cacheName).then(cache => {
-                        if (!evt.request.url.endsWith('sw.js')) {
-                            cache.put(evt.request.url, fetchRes.clone());
-                        }
-                        return fetchRes;
-                    });
+self.addEventListener('fetch', function(event) {
+    // Check if the request is a GET request.
+    if (event.request.method === "GET") {
+        event.respondWith(
+            fetch(event.request).then(function(response) {
+                // If a valid response is received, clone it and store it in the cache.
+                var responseClone = response.clone();
+                caches.open(cacheName).then(function(cache) {
+                    cache.put(event.request, responseClone);
+                });
+                return response;
+            }).catch(function() {
+                // On failure, try to return the cached response.
+                return caches.match(event.request).then(function(response) {
+                    if (response) {
+                        return response;
+                    }
+                    // If the request is for a navigation to a new page,
+                    // return the offline.html page if available in the cache.
+                    if (event.request.mode === 'navigate') {
+                        return caches.match('offline.html');
+                    }
                 });
             })
         );
